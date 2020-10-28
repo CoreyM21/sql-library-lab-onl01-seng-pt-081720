9c45263ce433f8f404e57d52484abd2e8509fc46
Corey Martin <coreyjmartin2@gmail.com>
2020-10-28 03:05:01 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,7 +35,22 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+"
+SELECT series.title
+ FROM series
+INNER JOIN books 
+ ON books.series_id = series.id
+INNER JOIN character_books
+ ON character_books.book_id = books.id
+ 
+INNER JOIN characters
+ ON characters.id = character_books.character_id
+  WHERE species = 'human'
+  GROUP BY series.title
+  LIMIT 1  
+;"
+#you're inside a string here.. so be careful- NO DOUBLE QUOTING use single quotes for values
+#ALSO, I can't see how to ORDER BY COUNT(characters.species = 'human') or whatever..
 end
 
 def select_character_names_and_number_of_books_they_are_in
